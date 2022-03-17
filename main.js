796f0a9845a785ebd6c65bca4b7a014c74f2406a
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2022-03-16 20:28:56 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+class Blocks {
+  getInfo () {
+    return {
+      id: 'someBlocks',
+      name: 'Some Blocks',
+      blocks: [
+        {
+          opcode: 'getJS',
+          blockType: BlockType.REPORTER,
+          text: '[JS]',
+          arguments: {
+            JS: {
+                type: ArgumentType.STRING,
+                defaultValue: 'JS'
+            }
+          }
+        }, 
+        {
+          opcode: 'executeJS',
+          blockType: BlockType.COMMAND,
+          text: 'execute [evalObject]',
+          arguments: {
+            evalObject: {
+                type: ArgumentType.STRING,
+                defaultValue:'JS eval object'
+            }
+          }
+        }, 
+        {
+          opcode: 'executeJSReport',
+          blockType: BlockType.REPORTER,
+          text: 'get value [evalObject]',
+          arguments: {
+            evalObject: {
+                type: ArgumentType.STRING,
+                defaultValue:'JS eval object'
+            }
+          }
+        }
+      ]
+    };
+  };
+  getJS (JS) {
+    return eval('function () { JS }');
+  }
+  executeJS (evalObject) {
+    evalObject();
+  }
+  executeJSReport (evalObject) {
+    return evalObject();
+  }
+}
